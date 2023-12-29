55569508af20ac114ce709e8738ad5ec68e18dc2
DELL <hoangphuc31052002@gmail.com>
2023-12-29 15:14:30 +0700

    update

--- app/src/main/java/com/example/kiemtraand/database/DbHelper.java
+++ app/src/main/java/com/example/kiemtraand/database/DbHelper.java
@@ -9,16 +9,16 @@ import androidx.annotation.Nullable;
 public class DbHelper extends SQLiteOpenHelper {
 
     public DbHelper(@Nullable Context context) {
-        super(context, "QLSanPham", null, 1);
+        super(context, "QLSanPham", null, 3);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = "CREATE TABLE SanPham(MaSP INTEGER PRIMARY KEY AUTOINCREMENT, " + "TenSP TEXT, GiaTien REAL, Image TEXT)";
         db.execSQL(sql);
-        String data = "INSERT INTO SanPham VALUES(1, 'Salad', '20000', 'https://static01.nyt.com/images/2018/02/21/dining/00RICEGUIDE8/00RICEGUIDE8-master768.jpg?w=1280&q=75')," +
-                "(2, 'Pasta', '30000', 'https://static01.nyt.com/images/2018/02/21/dining/00RICEGUIDE8/00RICEGUIDE8-master768.jpg?w=1280&q=75)," +
-                "(3, 'Pizza Cheese', '60000', 'https://static01.nyt.com/images/2018/02/21/dining/00RICEGUIDE8/00RICEGUIDE8-master768.jpg?w=1280&q=75')";
+        String data = "INSERT INTO SanPham VALUES(1, 'Cơm trộn', '50000', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/rice-a941d59.jpg?quality=90&resize=556,505')," +
+                "(2, 'cơm', '30000', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/rice-a941d59.jpg?quality=90&resize=556,505')," +
+                "(3, 'cơm', '90000', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/rice-a941d59.jpg?quality=90&resize=556,505')";
         db.execSQL(data);
     }
 
